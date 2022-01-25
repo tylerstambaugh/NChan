043d35e8b5a00cac3ca77848351f4d00c77dc5a2
Ethan Hunt <ethunt703@gmail.com>
2022-01-25 18:50:10 -0500

    post class

---
+++ NChan.WebAPI/Data/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NChan.WebAPI.Data
+{
+    public class Post
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        string Title { get; set; }
+        [Required]
+        string Text { get; set; }
+        [Required]
+        List<Comment> comments { get; set; }
+        [Required]
+        List<Like> likes { get; set; }
+        Guid AuthorId { get; set; }
+    }
+}
