e037360570acd0d204eee64c5641c0b9d1128094
GavinAnkney <gavinmartinankney@gmail.com>
2022-01-25 18:43:19 -0500

    Added comment enitity

--- NChan.Data/Comment.cs
+++ NChan.Data/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,12 @@ namespace NChan.Data
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
 
+        [ForeignKey("Post"), Required]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
     }
 }
