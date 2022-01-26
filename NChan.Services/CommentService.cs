a2778393a976a096a5681b5603dfdc6a705a2c1c
GavinAnkney <gavinmartinankney@gmail.com>
2022-01-26 13:59:50 -0500

    Added comment services, models and controller

---
+++ NChan.Services/CommentService.cs
@@ -0,0 +1,92 @@
+﻿using NChan.Data;
+using NChan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NChan.Services
+{
+    public class CommentService
+    {
+        public bool CreateComment(CommentCreate model)
+        {
+            var entity =
+                new Comment()
+                {
+                    Id = model.Id,
+                    Text = model.Text
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Comments.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CommentListItem> GetComments()
+
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Comments
+                        .Select(
+                            e =>
+                                new CommentListItem
+                                {
+                                    Id = e.Id,
+                                    Text = e.Text
+                                }
+                                );
+                return query.ToArray();
+            }
+        }
+
+        public CommentDetail GetCommentById(int commentId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Comments
+                        .Single(e => e.Id == commentId);
+                return
+                    new CommentDetail
+                    {
+                        Id = entity.Id,
+                        Text = entity.Text
+                    };
+            }
+        }
+
+        public bool UpdateComment(CommentEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Comments
+                        .Single(e => e.Id == model.Id);
+                entity.Text = model.Text;
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteComment(int commentId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Comments
+                        .Single(e => e.Id == commentId);
+                ctx.Comments.Remove(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
